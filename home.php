f08279aaa2fdd279c51c9b5f5660938bd42deffa
Mehrab <mehrab@zizou.local>
2018-03-03 17:42:50 +0800

    Remove video section

--- home.php
+++ home.php
@@ -38,9 +38,6 @@
 
     <!-- Start Video Section -->
     <div class="fullscreen-bg">
-        <video loop muted autoplay poster="video/videoPoster.png" class="fullscreen-bg__video">
-          <source src="video/video.mp4" type="video/mp4">
-          </video>
           <div class="aboutUs">
               <h1 class="about-head text-center">ProteinShape</h1>
               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
