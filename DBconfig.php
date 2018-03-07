34e8a0cbbf88a09038324dd84b1b177b3eb668f0
Mehrab <mehrab@zizou.local>
2018-03-07 11:28:37 +0800

    Add DBconfig file

---
+++ DBconfig.php
@@ -0,0 +1,13 @@
+<?php
+//Define your host here.
+$hostName = "localhost";
+ 
+//Define your database name here.
+$databaseName = "abc";
+ 
+//Define your database username here.
+$username = "root";
+ 
+//Define your database password here.
+$password = "";
+?>
