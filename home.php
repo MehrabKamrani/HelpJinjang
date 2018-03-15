0fb63a6c948107ca8d242fb955c63aa87231ddef
Mehrab <mehrab@zizou.local>
2018-03-16 00:32:34 +0800

    Change the headline in homepage

--- home.php
+++ home.php
@@ -68,7 +68,7 @@
     <!-- Start Video Section -->
     <div class="fullscreen-bg">
           <div class="aboutUs">
-              <h1 class="about-head text-center">HELPJinjang</h1>
+              <h1 class="about-head text-center">Impact a life, transform a community</h1>
               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
