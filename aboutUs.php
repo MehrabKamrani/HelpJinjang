a572f8372c32c466db0b31c6d6df29dc82536436
Umar Adkhamov <35759152+UmarAdkham@users.noreply.github.com>
2018-03-16 11:47:06 +0800

    Change text and pictures

--- aboutUs.php
+++ aboutUs.php
@@ -35,100 +35,94 @@
     <!-- Start Home Section -->
 
     <div id="about"><!-- About Section -->
-        <div class="container">
+        <div class="container wrapper">
             <div class="about-wrapper row">
                 <aside class="about-img-section col-md-6 col-sm-4">
-                    <img class="img-responsive about-img" src="images/agn_from_doc.jpg" alt="about photo">
+                    <img class="img-responsive about-img" src="images/agnlogo.png" alt="about photo">
                 </aside>
                 <div class="about-text-section col-md-6 col-sm-8">
                     <h1 class="about-title">About AGN</h1>
                     <p class="about-paragraph">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ac tellus at orci lacinia aliquet. In ornare tellus condimentum libero tincidunt vehicula. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus ut enim diam. Nulla semper justo a nisi ullamcorper ultrices. Curabitur tristique quam quis massa lacinia cursus. Duis tortor risus, pellentesque eget enim sit amet, pellentesque pharetra risus.
+                     ACTS Global Networking (AGN) is a non-profit organization mainly focusing on education and social concern activities. One of the community activities that it has been involved in over the past fourteen years is its “street ministry” where the organization reaches out and helps the hurting and neglected children and youth in Jinjang. AGN has been extending support, counselling and meeting the material needs of this needful community for over fourteen years.
 
-                        Aenean consequat erat et nibh tincidunt, sit amet varius lectus auctor. Morbi blandit ex sapien, interdum dignissim orci consectetur eu. Aliquam lorem arcu, cursus quis eleifend pretium, porttitor eu mauris. Aliquam sollicitudin et diam et mollis. Proin a diam scelerisque, tincidunt dolor ac, egestas augue. Nam scelerisque imperdiet tempus. Nullam eget diam in turpis molestie suscipit nec vel arcu. Nunc vitae aliquet enim. Nunc at viverra felis. Duis quis fringilla arcu.
+                 </p>
 
-                    </p>
-                    <p class="about-paragraph">
-                        Aliquam rhoncus laoreet ante, id imperdiet massa ornare ac. Nam quis mollis justo. Aenean ac nibh id dui volutpat viverra. Curabitur non dolor dui. Morbi volutpat metus vitae lectus aliquet ullamcorper. Nulla vestibulum rutrum quam et tempor. Quisque volutpat nec massa eget molestie. Proin sollicitudin nibh augue, at consectetur nulla volutpat eu. Donec nec efficitur est. Aliquam ornare dolor eget condimentum condimentum. Nunc felis est, tincidunt in tincidunt sed, condimentum sit amet velit. Curabitur interdum imperdiet massa at sollicitudin. Pellentesque pretium tincidunt leo, facilisis dignissim justo dapibus et. Proin facilisis neque leo, lacinia iaculis sapien malesuada sit amet. Etiam sit amet libero semper, rhoncus risus vitae, tincidunt neque.
-                    </p>
-                </div>
-            </div> 
+             </div>
+         </div> 
+     </div>
+ </div><!-- / About Section -->
+
+
+<hr>
+
+ <div id="ourWork"><!-- Our Work Section -->
+    <div class="container wrapper-second">
+        <div class="ourWork-wrapper row">
+
+            <div class="about-text-section col-md-6 col-sm-8">
+                <h1 class="ourWork-title">Our Work</h1>
+                <p class="ourWork-paragraph">
+                   Our main objective is to open a great deal of possibilities for Jinjang Utara people to apply for a job they prefer.
+                   For the most part, it is harder to seek and find a job for women, especially for mothers with their kids under their custody, who are not always able to work full time and leave their houses for long time. Consequently, another objective is to ease the way to find a vacancy for the mothers, so they will not have to leave their houses for long time, or will not leave at all, if there is a job they can do from home. By working in a certain job place mothers would gain additional income for their family by this improving its financial condition. Furthermore, it would make it easier to pay for their children education, which will promise about a good future of Jinjang Utara.
+
+
+               </p>
+              
         </div>
-    </div><!-- / About Section -->
+        <aside class="ourWork-img-section col-md-6 col-sm-4">
+            <img class="img-responsive about-img" src="images/our-work.png" alt="our work">
+        </aside>
+    </div> 
+</div>
+</div><!-- / Our Work Section -->
 
+<hr>
 
+<div id="literacy"><!-- Literacy Section -->
+    <div class="container">
+        <div class="row literacy">
+            <div class="col-sm-12">
+                <h1 class="literacy-title">Literacy and Numeracy program</h1>
 
+                <p class="literacy-paragraph">
+                    Education has an important role to play in the eradication of poverty. Unfortunately for many families living in poverty, education is a luxury they cannot afford. This is a great tragedy, as education should be a basic human right that must be met. 
 
-    <div id="ourWork"><!-- Our Work Section -->
-        <div class="container">
-            <div class="about-wrapper row">
+                    The effects of poverty had resulted in some children growing up in poverty stricken homes, suffering more frequent and persistent health problems compared to children who grow up under better financial circumstances. Children raised under poverty noticeably, tend to miss school more often because of illness, level of stress at home, lack of child- care facilities, and unfortunately, sometimes, violence at home. As our nation progresses, there needs to be intervention programmes for the poor to ensure they are not left behind. 
+                    Even though schooling is free here in Malaysia, those who live in poverty still cannot afford to pay for their children’s miscellaneous fees such as activities, textbooks and uniforms, and their meals. Now in this age and era, each child needs a computer, and these items that we take for granted are not cheap for those who are destitute. 
 
-                <div class="about-text-section col-md-6 col-sm-8">
-                    <h1 class="ourWork-title">Our Work</h1>
-                    <p class="ourWork-paragraph">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ac tellus at orci lacinia aliquet. In ornare tellus condimentum libero tincidunt vehicula. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus ut enim diam. Nulla semper justo a nisi ullamcorper ultrices. Curabitur tristique quam quis massa lacinia cursus. Duis tortor risus, pellentesque eget enim sit amet, pellentesque pharetra risus.
+                    Sadly, those who are destitute would rather send their children out to work, so that they can earn a living to support their parents and siblings. These children should remain in school, as it is proven that each year they remain in school their earning potential increases by 10-20%.  
 
-                        s 
+                    English may not be the most spoken language in the world, but it remains as the dominant language for business and has become almost a necessity for people to enter a global workforce. Its importance in the global market place therefore cannot be understated, as learning English really can change lives.
+                    Hence, the literacy project was launched in December 2014, giving opportunities for the resident children to unlock their full potential and find meaningful careers in the future. 
+                    The children in the Jinjang community are especially weak in the foundational subjects of English, Maths and Science. The Maths Numeracy Project for the children from Jinjang Utara then followed suit in early 2017. 
 
-                    </p>
-                    <p class="ourWork-paragraph">
-                        Aliquam rhoncus laoreet ante, id imperdiet massa ornare ac. Nam quis mollis justo. Aenean ac nibh id dui volutpat viverra. Curabitur non dolor dui. Morbi volutpat metus vitae lectus aliquet ullamcorper. Nulla vestibulum rutrum quam et tempor. Quisque volutpat nec massa eget molestie. Proin sollicitudin nibh augue, at consectetur nulla volutpat eu. Donec nec efficitur est. Aliquam ornare dolor eget condimentum condimentum. Nunc felis est, tincidunt in tincidunt sed, condimentum sit amet velit. Curabitur interdum imperdiet massa at sollicitudin. Pellentesque pretium tincidunt leo, facilisis dignissim justo dapibus et. Proin facilisis neque leo, lacinia iaculis sapien malesuada sit amet. Etiam sit amet libero semper, rhoncus risus vitae, tincidunt neque.
-                    </p>
-                </div>
-                <aside class="ourWork-img-section col-md-6 col-sm-4">
-                    <img class="img-responsive about-img" src="images/agn.jpg" alt="about photo">
-                </aside>
-            </div> 
-        </div>
-    </div><!-- / Our Work Section -->
+                    We are committed to do what we can to improve the literacy and numeracy skills of those who are left behind. 
 
+                    Although some supporters have donated generously each year to charity, the needs of the poor cannot all be met. There are only so much we can do. Many households in destitute require financial aid for many things including tuition and remedial classes for subjects that they are weak in.
 
+                    This is a small project, but one that must continue and sustain itself, so that it will have a greater impact on these children’s lives. We encourage you to join us in our effort in uplifting our communities and making a difference in their lives. It stems from a simple conviction that we all share, that every child and every life matters, for our community and for the future of our nation.
 
-    <div id="literacy"><!-- Literacy Section -->
-        <div class="container">
-            <div class="row literacy">
-                <div class="col-sm-12">
-                    <h1 class="literacy-title">Literacy and Numeracy program</h1>
-
-                    <p class="literacy-paragraph">
-                        Education has an important role to play in the eradication of poverty. Unfortunately for many families living in poverty, education is a luxury they cannot afford. This is a great tragedy, as education should be a basic human right that must be met. 
-
-                        The effects of poverty had resulted in some children growing up in poverty stricken homes, suffering more frequent and persistent health problems compared to children who grow up under better financial circumstances. Children raised under poverty noticeably, tend to miss school more often because of illness, level of stress at home, lack of child- care facilities, and unfortunately, sometimes, violence at home. As our nation progresses, there needs to be intervention programmes for the poor to ensure they are not left behind. 
-                        Even though schooling is free here in Malaysia, those who live in poverty still cannot afford to pay for their children’s miscellaneous fees such as activities, textbooks and uniforms, and their meals. Now in this age and era, each child needs a computer, and these items that we take for granted are not cheap for those who are destitute. 
-
-                        Sadly, those who are destitute would rather send their children out to work, so that they can earn a living to support their parents and siblings. These children should remain in school, as it is proven that each year they remain in school their earning potential increases by 10-20%.  
-
-                        English may not be the most spoken language in the world, but it remains as the dominant language for business and has become almost a necessity for people to enter a global workforce. Its importance in the global market place therefore cannot be understated, as learning English really can change lives.
-                        Hence, the literacy project was launched in December 2014, giving opportunities for the resident children to unlock their full potential and find meaningful careers in the future. 
-                        The children in the Jinjang community are especially weak in the foundational subjects of English, Maths and Science. The Maths Numeracy Project for the children from Jinjang Utara then followed suit in early 2017. 
-
-                        We are committed to do what we can to improve the literacy and numeracy skills of those who are left behind. 
-
-                        Although some supporters have donated generously each year to charity, the needs of the poor cannot all be met. There are only so much we can do. Many households in destitute require financial aid for many things including tuition and remedial classes for subjects that they are weak in.
-
-                        This is a small project, but one that must continue and sustain itself, so that it will have a greater impact on these children’s lives. We encourage you to join us in our effort in uplifting our communities and making a difference in their lives. It stems from a simple conviction that we all share, that every child and every life matters, for our community and for the future of our nation.
-
-                        “Life is not a competition. Life is about helping and inspiring others, so we can reach our full potential”
-                    </p>
-                </div>
+                    “Life is not a competition. Life is about helping and inspiring others, so we can reach our full potential”
+                </p>
             </div>
-           
         </div>
-    </div><!-- / Leteracy Section -->
 
-  
-
-   <?php include "footer.php"; ?>
+    </div>
+</div><!-- / Leteracy Section -->
 
 
-    <!-- jQuery -->
-    <script src="js/jquery-2.2.3.min.js"></script>
 
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
+<?php include "footer.php"; ?>
 
-    <!-- Lightbox JavaScript -->
-    <script src="js/lightbox.min.js"></script>
+
+<!-- jQuery -->
+<script src="js/jquery-2.2.3.min.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="js/bootstrap.min.js"></script>
+
+<!-- Lightbox JavaScript -->
+<script src="js/lightbox.min.js"></script>
 
 
 </body>
